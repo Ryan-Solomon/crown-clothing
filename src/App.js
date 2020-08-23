785806b2dc649a6b1efcff1e5811488933efbf32
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-08-23 10:46:56 -0500

    added collection-preview component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from '../src/pages/shop-page/shop-page.component';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path='/' component={HomePage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop-page' component={ShopPage} />
       </Switch>
     </div>
   );
